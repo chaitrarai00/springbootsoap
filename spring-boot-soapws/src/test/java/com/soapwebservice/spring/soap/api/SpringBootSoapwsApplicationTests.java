5405513fa32f15706ba1f8b70318c7aacc76b48a
chaitrarai00 <32186609+chaitrarai00@users.noreply.github.com>
2021-04-06 20:38:56 +0530

    Springboot soap skeleton

---
+++ spring-boot-soapws/src/test/java/com/soapwebservice/spring/soap/api/SpringBootSoapwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soapwebservice.spring.soap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
